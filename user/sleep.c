9ab2996c6a41b66b88e0779b77ebb92d70452969
realLiuzh <1138423425@qq.com>
2022-09-24 05:19:37 +0000

    Lab1:sleep finished

---
+++ user/sleep.c
@@ -0,0 +1,30 @@
+//1.获取用户输入的命令行参数
+//2.调用 sleep() System call
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int 
+main(int argc,char *argv[])
+{
+	int i,len;
+	// 参数校验
+	if(argc!=2){
+	  write(1,"input error!\n\0",14);
+	  exit(1);
+	} 
+	char* timeStr=argv[1];
+	char* temp=timeStr;
+	for(i=0,len=strlen(timeStr);i<len;i++){
+	   if('0'<=*(temp+i) && *(temp+i)<='9')
+	     continue;
+	   else {
+	     write(1,"input error!\n\0",14);
+	     exit(0);
+	   } 
+	}
+        // 系统调用
+	int time=atoi(timeStr);
+	sleep(time);	
+	exit(0);
+}
