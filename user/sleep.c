7dceaf107ede01c92eca13b68c1c3b13b70f1400
志昊的刘 <1138423425@qq.com>
2022-09-25 14:20:04 +0800

    docs:格式调整

--- user/sleep.c
+++ user/sleep.c
@@ -4,27 +4,26 @@
 #include "kernel/stat.h"
 #include "user/user.h"
 
-int 
-main(int argc,char *argv[])
-{
-	int i,len;
-	// 参数校验
-	if(argc!=2){
-	  write(1,"input error!\n\0",14);
-	  exit(1);
-	} 
-	char* timeStr=argv[1];
-	char* temp=timeStr;
-	for(i=0,len=strlen(timeStr);i<len;i++){
-	   if('0'<=*(temp+i) && *(temp+i)<='9')
-	     continue;
-	   else {
-	     write(1,"input error!\n\0",14);
-	     exit(0);
-	   } 
-	}
-        // 系统调用
-	int time=atoi(timeStr);
-	sleep(time);	
-	exit(0);
+int
+main(int argc, char *argv[]) {
+    int i, len;
+    // 参数校验
+    if (argc != 2) {
+        write(1, "input error!\n\0", 14);
+        exit(1);
+    }
+    char *timeStr = argv[1];
+    char *temp = timeStr;
+    for (i = 0, len = strlen(timeStr); i < len; i++) {
+        if ('0' <= *(temp + i) && *(temp + i) <= '9')
+            continue;
+        else {
+            write(1, "input error!\n\0", 14);
+            exit(0);
+        }
+    }
+    // 系统调用
+    int time = atoi(timeStr);
+    sleep(time);
+    exit(0);
 }
